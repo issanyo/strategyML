e965d79101e867059883903b0c34b165189caf32
Christian Kobril <christiankobril@gmail.com>
2021-11-30 12:53:30 +0100

    create keeper data schema

--- keeper/settings.py
+++ keeper/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'keeperbot.apps.KeeperbotConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- keeperbot/models.py
+++ keeperbot/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Data(models.Model):
+    price = models.FloatField()
+    volume = models.FloatField()
+    liquidity = models.FloatField()
+    timestamp = models.DateTimeField()
+    fees_pool_level = models.FloatField(verbose_name = 'fees pool level')
+    fees_vault = models.FloatField(verbose_name = 'fees vault')
+    il = models.FloatField(verbose_name  = 'IL')
