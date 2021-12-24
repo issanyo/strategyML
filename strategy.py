2755f714632bc8b0e79d381516db4fc689fb6bd6
ihsan younes <ihsan.younes@wegift.io>
2021-12-24 18:23:39 +0100

    fix env vars

--- strategy.py
+++ strategy.py
@@ -10,57 +10,51 @@ from datetime import datetime, timedelta
 
 
 def rebalance(limit_lower, base_lower, strategy, web3, keeper, pk):
-    print('Rebalancing')
+    print('Rebalancing...')
 
-    try:
+    print('setBaseThreshold')
+    nonce = web3.eth.getTransactionCount(keeper)
 
-        print('setBaseThreshold')
-        nonce = web3.eth.getTransactionCount(keeper)
+    tx = strategy.functions.setBaseThreshold(base_lower).buildTransaction(
+    {
+        'value': 0,
+        'from': keeper,
+        'chainId': 3,
+        'nonce': nonce
+    })
 
-        tx = strategy.functions.setBaseThreshold(base_lower).buildTransaction(
-        {
-            'value': 0,
-            'from': keeper,
-            'chainId': 3,
-            'nonce': nonce
-        })
-                
-        signed_tx = web3.eth.account.sign_transaction(tx, private_key = pk)
-        tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-        tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
-        
+    signed_tx = web3.eth.account.sign_transaction(tx, private_key = pk)
+    tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
 
-        print('setLimitThreshold')
-        nonce = web3.eth.getTransactionCount(keeper)
-        tx = strategy.functions.setLimitThreshold(limit_lower).buildTransaction(
-        {
-            'value': 0,
-            'chainId': 3,
-            'from': keeper,
-            'nonce': nonce
-        })
-        signed_tx = web3.eth.account.sign_transaction(tx, private_key = pk)
-        tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-        tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
 
-        print('rebalance tx')
-        nonce = web3.eth.getTransactionCount(keeper)
-        tx = strategy.functions.rebalance().buildTransaction(
-        {
-            'value': 0,
-            'chainId': 3,
-            'from': keeper,
-            'nonce': nonce
-        })
-        signed_tx = web3.eth.account.sign_transaction(tx, private_key = pk)
-        tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-        tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+    print('setLimitThreshold')
+    nonce = web3.eth.getTransactionCount(keeper)
+    tx = strategy.functions.setLimitThreshold(limit_lower).buildTransaction(
+    {
+        'value': 0,
+        'chainId': 3,
+        'from': keeper,
+        'nonce': nonce
+    })
+    signed_tx = web3.eth.account.sign_transaction(tx, private_key = pk)
+    tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
 
-        print('Rebalance successful')
-    except Exception as e:
-        print(e)
-        print('Rebalance failed')
-    return
+    print('rebalance tx')
+    nonce = web3.eth.getTransactionCount(keeper)
+    tx = strategy.functions.rebalance().buildTransaction(
+    {
+        'value': 0,
+        'chainId': 3,
+        'from': keeper,
+        'nonce': nonce
+    })
+    signed_tx = web3.eth.account.sign_transaction(tx, private_key = pk)
+    tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+
+    print('Rebalance successful, transaction: ', tx_receipt)
 
 
 def fetch_and_rebalance(network, infura_url, keeper, pk):
