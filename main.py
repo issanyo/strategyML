42849772a23a67b40b10cf9fa8f16dbff3fe5aa4
ihsan younes <ihsan.younes@wegift.io>
2021-12-26 14:37:01 +0100

    fix env variables

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import os
 from .strategy_brownie import fetch_and_rebalance
 
 def main():
-    fetch_and_rebalance(os.environ['VAULT_CONFIG'], os.environ['ROPSTEN_KEEPER'], os.environ['ROPSTEN_PK'])
+    fetch_and_rebalance(os.environ['VAULT_CONFIG'], os.environ['KEEPER'], os.environ['PK'])
