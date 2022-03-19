faa30b15be36a6abcd0494a92d478166eafba76c
Ihsan Younes <ihsan@beacon.co.uk>
2022-03-19 14:41:23 +0100

    addresses polygon

--- exec_polygon.py
+++ exec_polygon.py
@@ -1,3 +1,3 @@
 from main import main
 
-main("0x34b97ffa01dc0DC959c5f1176273D0de3be914C1", "0x741e3E1f81041c62C2A97d0b6E567AcaB09A6232", 'polygon-main', False)
+main("0x1B94C4EC191Cc4D795Cd0f0929C59cA733b6E636", "0x9f235C8d5BEfee9Bb077E56dCde32c9bAe2F4180", 'polygon-main', False)
