bb1d4c23032ab90513e9d08272d0e276c63e995a
Ihsan Younes <ihsan@beacon.co.uk>
2022-03-31 09:26:38 +0200

    fix slippage

--- strategy.py
+++ strategy.py
@@ -27,7 +27,7 @@ def calculate_amounts_to_rebalance(vault_data, tokens):
     # if 75% of funds are already balanced, do not perform swaps
     TOKEN0_THRESHOLD = half_tvl * 0.25
     TOKEN1_THRESHOLD = half_tvl * 0.25 / vault_data['price']
-    SLIPPAGE = 10
+    SLIPPAGE = 5
 
     token0q = vault_data['total0'] - half_tvl
     token1q = vault_data['total1'] - half_tvl / vault_data['price']
@@ -43,11 +43,11 @@ def calculate_amounts_to_rebalance(vault_data, tokens):
     if token1q > 0 and token1q > TOKEN1_THRESHOLD:
         # from token1 to token0 (negative)
         amountToSwap = - token1q * (10 ** tokens[1].decimals())
-        sqrtPriceLimitX96 = getSqrtRatioAtTick(to_tick(vault_data['price'] + SLIPPAGE, tokens))
+        sqrtPriceLimitX96 = getSqrtRatioAtTick(to_tick(vault_data['price'] - SLIPPAGE, tokens))
     # buy
     elif token0q > 0 and token0q > TOKEN0_THRESHOLD:
         # from token0 to token1 (positive)
         amountToSwap = token0q * (10 ** tokens[0].decimals())
-        sqrtPriceLimitX96 = getSqrtRatioAtTick(to_tick(vault_data['price'] - SLIPPAGE, tokens))
+        sqrtPriceLimitX96 = getSqrtRatioAtTick(to_tick(vault_data['price'] + SLIPPAGE, tokens))
 
     return [amountToSwap, sqrtPriceLimitX96]
