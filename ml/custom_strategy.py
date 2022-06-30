6f98e7a5117f986cf246ab95e4cc101fbc11ef4b
Ihsan Younes <ihsan@beacon.co.uk>
2022-06-30 07:01:23 +0100

    SimpleStrategy 207.01186521811462, 0.32601112958747525

--- ml/custom_strategy.py
+++ ml/custom_strategy.py
@@ -8,14 +8,12 @@ class SimpleStrategy:
     2 -> decrement
     3 -> recenter with current settings
     """
-    RANGES = [0, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2, 0.3, 0.4, 0.6, 0.8]
-
     def predict(self, state):
         action = 0
 
         data = list(state.as_numpy_iterator())[0][0]
         # data = state
-        q = len(data) // 2
+        q = len(data) // 4
 
         deviation = data[:, 0].std()
         mean = np.sqrt((data[:, 0] ** 2).mean())
@@ -45,14 +43,7 @@ class SimpleStrategy:
         p_above_curr_range = np.sum((data[:, 3] == 0) & (data[:, 0] > 0)) / len(data[:, 3])
         p_below_curr_range = np.sum((data[:, 3] == 0) & (data[:, 0] < 0)) / len(data[:, 3])
 
-        trend_last_quartile = np.abs(
-            np.average([data[i, 0] / (data[i, 1] / 2) for i in range(len(data) - q, len(data))],
-                       weights=np.arange(1, q + 1) / (q * (q + 1) / 2)))
-
-        # mean_reward >= -0.001 # are we paying too much for rebalance?
-
         print("currently_inside_range", currently_inside_range)
-        print("trend_last_quartile", trend_last_quartile)
         print("p_reward_last_quartile", p_reward_last_quartile)
         print("deviation_last_quartile", deviation_last_quartile)
         print("fees_last_quartile", fees_last_quartile)
@@ -63,27 +54,22 @@ class SimpleStrategy:
         print("p_below_curr_range", p_below_curr_range)
 
         # we are outside range and the price is not too volatile
-        if fees_last_quartile < 0.1:
+        if fees_last_quartile < 0.12:
 
-            if not currently_inside_range and 0.1 < p_reward < 0.75 and (
-                    p_above_curr_range * p_below_curr_range > 0.001 or (
-                    p_reward_last_quartile > 0.9 and p_outside_curr_range > 0.5)):
+            if not currently_inside_range and (deviation_last_quartile > 0.6 or (0.05 < p_reward < 0.65 and (
+                    p_above_curr_range * p_below_curr_range > 1e-05 or (
+                    p_reward_last_quartile > 0.4 and p_outside_curr_range > 0.8)))):
                 # increment, outsiders up and down
                 print("SimpleStrategy if#1")
                 action = 1
-            elif p_reward > 0.99 and p_reward_last_quartile > 0.99 and deviation < 0.1:
+            elif p_reward > 0.99 and p_reward_last_quartile > 0.99 and deviation < 0.2:
                 # decrement, majourity of values are inside
                 print("SimpleStrategy if#2")
                 action = 2
 
             # check if we are still inside the current range
-            elif deviation_last_quartile <= 0.25 and not currently_inside_range and p_reward_last_quartile < 0.5:  # mean > curr_range and
+            elif deviation_last_quartile <= 1.0 and not currently_inside_range and p_reward_last_quartile < 0.2: #mean > curr_range and
                 print("SimpleStrategy if#3")
-                action = 3
-
-            # antecipate rebalance to prevent IL
-            elif trend_last_quartile > 0.9:  # and deviation_last_quartile > 1:
-                print("SimpleStrategy if#4")
                 action = 3
 
         a = np.zeros(4)
